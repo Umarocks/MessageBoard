1e2f6063cee829868d857ea345c12133b1622b21
Umarocks <umarocks@hotmail.com>
2024-01-15 15:06:39 -0600

    Working and am able to show messages on the web

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import express from "express";
+import exphbsModule from "express-handlebars";
+
+const exphbs = exphbsModule.create();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const router = express.Router();
+
+app.engine("handlebars", exphbs.engine);
+app.set("view engine", "handlebars");
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index.handlebars", { messages: messages });
 });
 
 app.listen(PORT, () => {
